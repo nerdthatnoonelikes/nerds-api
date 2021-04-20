18d84bda09a308fea7a0f92504896621e63c22c5
nerdthatnoonelikes <kenrag24@gmail.com>
2021-04-20 09:04:30 -0500

    update shorten route

--- src/routes/ShortenRoute.ts
+++ src/routes/ShortenRoute.ts
@@ -1,15 +1,17 @@
 import { Request, Response, Router } from "express";
 import Link from "../models/Link";
 import validUrl from "valid-url";
+import short from "shortid";
 
 const router: Router = Router();
 
 router.post("/api/v1/shorten", (req: Request, res: Response) => {
+  const id = short.generate();
   if (validUrl.isUri(req.body.redirect)) {
     Link.create({
-      id: req.body.id,
+      id: id,
       redirect: req.body.redirect,
-    }).then(() => res.send("saved to db"));
+    }).then(() => res.json({ id: id, redirect: req.body.redirect }));
   } else {
     return res.send("That is not a valid URL");
   }
